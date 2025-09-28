dd500217416fde919c2a21887ba7dad3ebd32d1f
DainisFyodorov <fedorovdainis@gmail.com>
2025-09-28 12:07:42 +0300

    User registration - skills entity, creation of recruiter/job seeker profile implemented

---
+++ src/main/java/com/dainis/jobportal/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,7 @@
+package com.dainis.jobportal.repository;
+
+import com.dainis.jobportal.entity.JobSeekerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Integer> {
+}
---
+++ src/main/java/com/dainis/jobportal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,7 @@
+package com.dainis.jobportal.repository;
+
+import com.dainis.jobportal.entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+}
