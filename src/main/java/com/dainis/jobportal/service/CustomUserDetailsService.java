9ad6954273aab99fd4385f8de31cfabcf7a4d0b9
DainisFyodorov <fedorovdainis@gmail.com>
2025-09-29 11:38:25 +0300

    Spring Security custom authentication and authorization implemented

---
+++ src/main/java/com/dainis/jobportal/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.dainis.jobportal.service;
+
+import com.dainis.jobportal.entity.Users;
+import com.dainis.jobportal.repository.UsersRepository;
+import com.dainis.jobportal.util.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users users = usersRepository.findByEmail(username).orElseThrow(() -> new
+                UsernameNotFoundException("Could not found user"));
+        return new CustomUserDetails(users);
+    }
+}
