9bab48ade8a609720b2cf7aae5b1bfe2f8c498d9
DainisFyodorov <fedorovdainis@gmail.com>
2025-10-04 10:39:40 +0300

    Recruiter profile - controller and service implemented

---
+++ src/main/java/com/dainis/jobportal/service/RecruiterProfileService.java
@@ -0,0 +1,23 @@
+package com.dainis.jobportal.service;
+
+import com.dainis.jobportal.entity.RecruiterProfile;
+import com.dainis.jobportal.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+
+    private final RecruiterProfileRepository recruiterProfileRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id) {
+        return recruiterProfileRepository.findById(id);
+    }
+}
