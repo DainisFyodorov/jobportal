cc9ad7efac997d290821d0fd8eec3b137bd449b2
DainisFyodorov <fedorovdainis@gmail.com>
2025-10-08 11:05:56 +0300

    Displaying and retrieving jobs in dashboard implemented

--- src/main/java/com/dainis/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/dainis/jobportal/controller/JobPostActivityController.java
@@ -1,12 +1,15 @@
 package com.dainis.jobportal.controller;
 
 import com.dainis.jobportal.entity.JobPostActivity;
+import com.dainis.jobportal.entity.RecruiterJobsDto;
+import com.dainis.jobportal.entity.RecruiterProfile;
 import com.dainis.jobportal.entity.Users;
 import com.dainis.jobportal.service.JobPostActivityService;
 import com.dainis.jobportal.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
@@ -36,6 +40,12 @@ public class JobPostActivityController {
         if(!(authentication instanceof AnonymousAuthenticationToken)) {
             String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))) {
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.
+                        getRecruiterJobs(((RecruiterProfile) currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
 
         model.addAttribute("user", currentUserProfile);
--- src/main/java/com/dainis/jobportal/service/JobPostActivityService.java
+++ src/main/java/com/dainis/jobportal/service/JobPostActivityService.java
@@ -1,9 +1,12 @@
 package com.dainis.jobportal.service;
 
-import com.dainis.jobportal.entity.JobPostActivity;
+import com.dainis.jobportal.entity.*;
 import com.dainis.jobportal.repository.JobPostActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class JobPostActivityService {
@@ -17,5 +20,20 @@ public class JobPostActivityService {
 
     public JobPostActivity addNew(JobPostActivity jobPostActivity) {
         return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter) {
+
+        List<IRecruiterJobs> recruiterJobsDtos = jobPostActivityRepository.getRecruiterJobs(recruiter);
+        List<RecruiterJobsDto> recruiterJobsDtoList = new ArrayList<>();
+
+        for(IRecruiterJobs rec :  recruiterJobsDtos){
+            JobLocation location = new JobLocation(rec.getLocationId(), rec.getCity(), rec.getState(), rec.getCountry());
+            JobCompany company = new JobCompany(rec.getCompanyId(), rec.getName(), "");
+            recruiterJobsDtoList.add(new RecruiterJobsDto(rec.getTotalCandidates(), rec.getJob_post_id(),
+                    rec.getJob_title(), location, company));
+        }
+
+        return recruiterJobsDtoList;
     }
 }
