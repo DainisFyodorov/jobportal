f55226d1c673b8f1daf6608d9871975893f9d780
DainisFyodorov <fedorovdainis@gmail.com>
2025-10-10 19:29:12 +0300

    Job Seeker Profile implemented - controller and service

---
+++ src/main/java/com/dainis/jobportal/controller/JobSeekerProfileController.java
@@ -0,0 +1,87 @@
+package com.dainis.jobportal.controller;
+
+import com.dainis.jobportal.entity.JobSeekerProfile;
+import com.dainis.jobportal.entity.Skills;
+import com.dainis.jobportal.entity.Users;
+import com.dainis.jobportal.repository.JobSeekerProfileRepository;
+import com.dainis.jobportal.repository.UsersRepository;
+import com.dainis.jobportal.service.JobSeekerProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/job-seeker-profile")
+public class JobSeekerProfileController {
+
+    private final UsersRepository usersRepository;
+    private final JobSeekerProfileService jobSeekerProfileService;
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+
+    @Autowired
+    public JobSeekerProfileController(UsersRepository usersRepository, JobSeekerProfileService jobSeekerProfileService, JobSeekerProfileRepository jobSeekerProfileRepository) {
+        this.usersRepository = usersRepository;
+        this.jobSeekerProfileService = jobSeekerProfileService;
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+    }
+
+    @GetMapping("/")
+    public String JobSeekerProfile(Model model) {
+
+        JobSeekerProfile jobSeekerProfile = new JobSeekerProfile();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<Skills> skills = new ArrayList<>();
+
+        if(!(authentication instanceof AnonymousAuthenticationToken)) {
+            Users user = usersRepository.findByEmail(authentication.getName())
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            Optional<JobSeekerProfile> seekerProfile = jobSeekerProfileService.getOne(user.getUserId());
+
+            if(seekerProfile.isPresent()) {
+                jobSeekerProfile = seekerProfile.get();
+                if(jobSeekerProfile.getSkills().isEmpty()) {
+                    skills.add(new Skills());
+                    jobSeekerProfile.setSkills(skills);
+                }
+            }
+
+            model.addAttribute("skills", skills);
+            model.addAttribute("profile", jobSeekerProfile);
+        }
+
+        return "job-seeker-profile";
+    }
+
+    @PostMapping("/addNew")
+    public String addNew(JobSeekerProfile jobSeekerProfile,
+                         @RequestParam("image") MultipartFile image,
+                         @RequestParam("pdf") MultipartFile pdf,
+                         Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(!(authentication instanceof AnonymousAuthenticationToken)) {
+            Users user = usersRepository.findByEmail(authentication.getName())
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+            jobSeekerProfile.setUserId(user);
+            jobSeekerProfile.setUserAccountId(user.getUserId());
+        }
+
+        List<Skills> skillsList = new ArrayList<>();
+        model.addAttribute("profile", jobSeekerProfile);
+        model.addAttribute("skills", skillsList);
+
+        return "redirect:/dashboard/";
+    }
+}
---
+++ src/main/java/com/dainis/jobportal/service/JobSeekerProfileService.java
@@ -0,0 +1,23 @@
+package com.dainis.jobportal.service;
+
+import com.dainis.jobportal.entity.JobSeekerProfile;
+import com.dainis.jobportal.repository.JobSeekerProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class JobSeekerProfileService {
+
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+
+    @Autowired
+    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+    }
+
+    public Optional<JobSeekerProfile> getOne(Integer id) {
+        return jobSeekerProfileRepository.findById(id);
+    }
+}
